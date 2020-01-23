67bd5728904365d04338f4ea784c0cd877b56f33
Zakariya Ahmed <zakahmed@umich.edu>
2020-01-23 14:49:13 -0500

    Finish assigment 2. was easy as pie

---
+++ Ass2/god damn it all to hifil/god damn it all to hifil/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace god_damn_it_all_to_hifil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Income = new System.Windows.Forms.TextBox();
+            this.Detuctions = new System.Windows.Forms.TextBox();
+            this.Sumbit = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Income
+            // 
+            this.Income.Location = new System.Drawing.Point(84, 58);
+            this.Income.Name = "Income";
+            this.Income.Size = new System.Drawing.Size(213, 20);
+            this.Income.TabIndex = 0;
+            this.Income.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Detuctions
+            // 
+            this.Detuctions.Location = new System.Drawing.Point(84, 106);
+            this.Detuctions.Name = "Detuctions";
+            this.Detuctions.Size = new System.Drawing.Size(213, 20);
+            this.Detuctions.TabIndex = 1;
+            // 
+            // Sumbit
+            // 
+            this.Sumbit.Location = new System.Drawing.Point(420, 58);
+            this.Sumbit.Name = "Sumbit";
+            this.Sumbit.Size = new System.Drawing.Size(299, 68);
+            this.Sumbit.TabIndex = 2;
+            this.Sumbit.Text = "Sumbit Taxes(if dectuion is empty than assume it\'s 12200)";
+            this.Sumbit.UseVisualStyleBackColor = true;
+            this.Sumbit.Click += new System.EventHandler(this.Sumbit_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(191, 213);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(438, 178);
+            this.Output.TabIndex = 3;
+            this.Output.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Income";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Detuctions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Sumbit);
+            this.Controls.Add(this.Detuctions);
+            this.Controls.Add(this.Income);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Income;
+        private System.Windows.Forms.TextBox Detuctions;
+        private System.Windows.Forms.Button Sumbit;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ass2/god damn it all to hifil/god damn it all to hifil/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace god_damn_it_all_to_hifil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    public struct TaxBrack{
+			public double Bracket;
+			public int LRange;
+			public int HRange;
+
+			public TaxBrack(double A, int B, int C){
+				Bracket=A;
+				LRange=B;
+				HRange=C;
+			}
+		}
+
+        static string PRINT = "";
+		// The code for calcuatoing the Hrange gets complex/hard to read, so I'm making my own function for it, so when I need to change it, I change it here.
+		public static double TaxNum(int H, int L, double B ){
+			double sum=Math.Round(B* (H-L));
+			PRINT+=("At "+B+"% bracket, the amount due is  "+sum +"\n");
+			return (sum);
+		}
+		// Given amount of taxable income, return the tax of it, from the highest bracket for a single person
+		public static void FederalTax(int TaxableIncome, int Det=12200){
+			int temp=TaxableIncome;
+			TaxableIncome-=Det;
+
+			//Console.Write(TaxableIncome+":\t");
+			TaxBrack[] Taxis={
+				new TaxBrack(.10,0,9700),		//0
+				new TaxBrack(.12,9700,39475),		//1
+				new TaxBrack(.22,39475,84200),		//2
+				new TaxBrack(.24,84200,160725),		//3
+				new TaxBrack(.32,160725,204100),	//4
+				new TaxBrack(.35,204100,510300),	//5
+				new TaxBrack(.37, 510300,-1) 		//6
+			};
+
+			int Range=0;
+			for(int i=1; i<Taxis.Length; i++){
+				if(Taxis[i].LRange<TaxableIncome){
+					Range++;
+				}
+			}
+			PRINT+=(Range +"\t" + Taxis[Range].Bracket +"\n");
+
+			double sum=0;
+			for(int i=0; i<Range; i++){
+				sum+=TaxNum(Taxis[i].HRange, Taxis[i].LRange, Taxis[i].Bracket);
+			}
+			sum+=TaxNum(TaxableIncome, Taxis[Range].LRange, Taxis[Range].Bracket);
+
+			for(int i=Range; i<Taxis.Length; i++){
+
+				PRINT+=("At "+Taxis[i].Bracket+"% bracket, the amount due is  "+0 +"\n");
+			}
+
+
+			if ( sum<0)
+				sum=0;
+
+			PRINT+=("Your tax is " + sum +
+					"$ with a gross income percentage of " + Math.Round(sum/temp,2)  
+					+ "% adjected it's " + Math.Round(sum/TaxableIncome,2) + "%\n" +
+
+					"At a dectuable rate of " + Det + "\n");
+
+
+
+		}
+        private void Sumbit_Click(object sender, EventArgs e)
+        {
+            string temp = Income.Text;
+            int IIncome = 0;
+            if (temp.Length != 0 ) {
+                IIncome = Int32.Parse(temp);
+                temp = Detuctions.Text;
+                if (temp.Length != 0)
+                {
+                    int Decutions = Int32.Parse(temp);
+                    FederalTax(IIncome, Decutions);
+                    Output.Text = PRINT;
+                    PRINT = "";
+                }
+                else {
+                    FederalTax(IIncome);
+                    Output.Text = PRINT;
+                    PRINT = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nothings in taxes BRAH");
+            }
+
+              
+        }
+    }
+}
